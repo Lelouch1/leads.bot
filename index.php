aee83f860a711cc976255469ba9621a13cbbd0a8
Dmitry <desing94root@gmail.com>
2021-06-05 20:53:53 +0300

    Add bot commands && README.MD

--- index.php
+++ index.php
@@ -2,9 +2,29 @@
 
 use app\ApiLeads;
 use GuzzleHttp\Client;
+use TelegramBot\Api\Client as TelegramClient;
 
 require './vendor/autoload.php';
 $config = require 'config.php';
 
-$a = new ApiLeads($config['token_webmaster'], new Client());
-var_dump($a->getCountry());
+$apiToken = $config['token_webmaster'];
+$botToken = $config['token_telegram'];
+
+
+try {
+    $bot = new TelegramClient($botToken);
+    $leadsApi = new ApiLeads($apiToken, new Client());
+
+    $bot->command('getaccountinfo', function ($message) use ($bot, $leadsApi) {
+        $bot->sendMessage($message->getChat()->getId(), $leadsApi->getAccountInfo());
+    });
+
+    $bot->command('getcountry', function ($message) use ($bot, $leadsApi) {
+        $bot->sendMessage($message->getChat()->getId(), $leadsApi->getCountry());
+    });
+
+    $bot->run();
+
+} catch (\TelegramBot\Api\Exception $e) {
+    echo $e->getMessage();
+}
